fde7b96ed4ab5e0a9f6fbe6b785e093a7be1d605
mbhossain <king.cseiu@gmail.com>
2022-06-19 20:52:49 +0600

    view encapsulation: angular default emulated> each html element contain unique attribute, none> no view encapsulation will be used, all child component contain same style withour any attribute

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ParentComponent } from './components/component-interaction/parent/parent.component';
 import { ChildComponent } from './components/component-interaction/child/child.component';
+import { ComponentTwoComponent } from './components/view-encapsulation/component-two/component-two.component';
+import { ComponentOneComponent } from './components/view-encapsulation/component-one/component-one.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ParentComponent,
-    ChildComponent
+    ChildComponent,
+    ComponentTwoComponent,
+    ComponentOneComponent
   ],
   imports: [
     BrowserModule
