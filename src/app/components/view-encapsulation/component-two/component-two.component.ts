f3c38fc004bef3fd46279dafeabfbfdf64cd30be
mbhossain <king.cseiu@gmail.com>
2022-06-19 21:08:41 +0600

    view encapsulation: shadow dom creates its won dom, it's separate from the main dom

--- src/app/components/view-encapsulation/component-two/component-two.component.ts
+++ src/app/components/view-encapsulation/component-two/component-two.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-component-two',
   templateUrl: './component-two.component.html',
-  styleUrls: ['./component-two.component.css']
+  styleUrls: ['./component-two.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class ComponentTwoComponent implements OnInit {
 
