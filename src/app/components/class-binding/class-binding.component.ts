d8b0a54fce453f8d7a7a49c15055db0090ffa49d
mbhossain <king.cseiu@gmail.com>
2022-07-23 15:19:02 +0600

    Class Binding

--- src/app/components/class-binding/class-binding.component.ts
+++ src/app/components/class-binding/class-binding.component.ts
@@ -2,10 +2,38 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-class-binding',
-  templateUrl: './class-binding.component.html',
-  styleUrls: ['./class-binding.component.css']
+  template: `
+  <h2>{{name}}</h2>
+  <h3 class="text-success">Test Feature</h3>
+  <h3 [class]="successClass">Test Feature</h3>
+
+  <h3 class="text-special" [class]="successClass">Test Feature</h3>
+  <h3 [class.text-danger]="hasError">Test Feature</h3>
+
+  <h3 [ngClass]="messageClasses">Test Feature</h3>
+  `,
+  styles: [`
+  .text-success {
+    color: green;
+  }
+  .text-danger {
+    color: red;
+  }
+  .text-special {
+    font-style: italic;
+  }
+  `]
 })
 export class ClassBindingComponent implements OnInit {
+  public name = 'Class Binding';
+  public successClass = 'text-success';
+  public hasError = true;
+  public isSpecial = true;
+  public messageClasses = {
+    'text-success': !this.hasError,
+    'text-danger': this.hasError,
+    'text-special': this.isSpecial
+  }
 
   constructor() { }
 
